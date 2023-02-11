ce4ee4e21c18ed188772e3ef1798702fb3858ec1
Forthoney <castlehoneyjung@gmail.com>
2023-02-10 23:11:06 -0500

    add username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :themes
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 end
